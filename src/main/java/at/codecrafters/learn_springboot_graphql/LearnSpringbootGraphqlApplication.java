1f33fcc1bd58a231f6355617e5b4b4a441715d36
wienm <martin.scheibelreiter@chello.at>
2025-01-16 22:08:50 +0100

    init

---
+++ src/main/java/at/codecrafters/learn_springboot_graphql/LearnSpringbootGraphqlApplication.java
@@ -0,0 +1,13 @@
+package at.codecrafters.learn_springboot_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringbootGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/codecrafters/learn_springboot_graphql/LearnSpringbootGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package at.codecrafters.learn_springboot_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
