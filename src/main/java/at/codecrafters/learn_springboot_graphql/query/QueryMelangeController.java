e09a426500be2bb7f256ea668d201861a0339bf3
wienm <martin.scheibelreiter@chello.at>
2025-01-17 23:06:43 +0100

    first Query und enable graphiql

---
+++ src/main/java/at/codecrafters/learn_springboot_graphql/query/QueryMelangeController.java
@@ -0,0 +1,23 @@
+package at.codecrafters.learn_springboot_graphql.query;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Controller
+public class QueryMelangeController {
+
+    @QueryMapping
+    public String firstQuery(){
+        System.out.println("First query called");
+        return "Hello World!";
+    }
+    //@QueryMapping, @SchemaMapping, und @MutationMapping
+
+
+}
